3a7bef546192c1b4c832743b471dbec2476ea2a5
prashantjha8603-jpg <prashantjha8603@gmail.com>
2026-08-03 13:58:35 +0530

    Added ElectionVoteCounter

--- java_06_arrays/ElectionVoteCounter.java
+++ java_06_arrays/ElectionVoteCounter.java
@@ -1,7 +1,36 @@
 package java_06_arrays;
 
+import java.util.Scanner;
+
 public class ElectionVoteCounter {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        int[] count = new int[5];
+        int spoilt = 0;
+
+        System.out.print("Enter total ballots: ");
+        int ballots = sc.nextInt();
+
+        for (int i = 0; i < ballots; i++) {
+
+            System.out.print("Enter candidate number : ");
+            int vote = sc.nextInt();
+
+            if (vote >= 1 && vote <= 5) {
+                count[vote - 1]++;
+            } else {
+                spoilt++;
+            }
+        }
+
+        System.out.println("\nvote count");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Candidate " + (i + 1) + " = " + count[i]);
+        }
+        System.out.println("Spoilt Ballots = " + spoilt);
+
+        sc.close();
     }
 }
